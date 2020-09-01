e009d5657e92540cb32332cd21c79c4c67fa8c4a
Dinky Acharya <dinkyacharya200@gmail.com>
2020-09-01 11:33:33 +0530

    test files

---
+++ square.test.js
@@ -0,0 +1,20 @@
+const { TestScheduler } = require("jest");
+
+var square = require("./square")
+test("Test Square of a number 2", ()=>{
+    expect(square(2)).toBe(4)
+});
+test("Test Square of a number -2", ()=>{
+    expect(square(-2)).toBe(4)
+});      
+test("Test Square of a number 0", ()=>{
+    expect(square(0)).toBe(0)
+});
+
+test("Test Square of a number 10", ()=>{
+    expect(square(10)).toBe(100)
+});
+
+test("Test Square of a number 0", ()=>{
+    expect(square(90)).toBe(8100)
+});
